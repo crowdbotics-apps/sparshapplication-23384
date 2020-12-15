51cfd0377e5559045327fc43b639374b7c8cacc2
Crowdbotics <team@crowdbotics.com>
2020-12-15 14:32:49 +0000

    Update models

--- sparshapplication_23384/wsgi.py
+++ sparshapplication_23384/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sparshapplication_23384.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sparshapplication_23384.settings")
 
 application = get_wsgi_application()
